905177515835e36c3640e520f9057fe24f42f5dd
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2023-02-15 09:52:54 -0600

    Converted rect to GeoObject

--- rect.js
+++ rect.js
@@ -1,37 +1,26 @@
-export default class Rect {
+import GeoObject from "./geoobject.js";
 
-    #data = new Float32Array(4);
+export default class Rect extends GeoObject {
 
     constructor(x, y, width, height) {
-        if(x instanceof Rect) this.#data.set()
-        else this.#data.set([x, y, x + width, y + height]);
+        super({params: ['x', 'y', 'width', 'height']});
+        if(x instanceof Rect) this.setValue(x);
+        else this.setValue(x, y, width, height);
     }
 
-    get x( ) {return this.#data[0]}
-    set x(n) {this.#data[0] = n}
+    get left( ) { return this.x }
+    set let(n)  {this.x = n}
 
-    get y( ) {return this.#data[0]}
-    set y(n) {this.#data[0] = n}
-
-    get width( ) {return this.#data[2] - this.#data[0]}
-    set width(n) {this.#data[2] = this.#data[0] + n}
-    
-    get height( ) {return this.#data[3] - this.#data[1]}
-    set height(n) {this.#data[3] = this.#data[1] + n}
-
-    get left( ) {return this.#data[0]}
-    set left(n) {this.x = n}
-
-    get right( ) {return this.#data[2]}
+    get right( ) {return this.x + this.width}
     set right(n) {this.x = n - this.width}
 
-    get top( ) {return this.#data[1]}
+    get top( ) {return this.y}
     set top(n) {this.y = n}
 
-    get bottom( ) {return this.#data[3]}
-    set bottom(n) {this.#data[1] = n - this.height}
+    get bottom( ) {return this.y + this.height}
+    set bottom(n) {this.y = n - this.height}
 
-    get topLeft( ) { return this.#data.subarray(0, 3)}
-    get bottomRight( ) {return this.#data.subarray(2, 4)}
+    get top_left( ) {return [this.x, this.y]}
 
+    get bottom_right( ) {return [this.x + this.width, this.y + this.height]}
 }
